1bfa1b692af7d9e815048d959459f5518cfba69e
Gustavo Tessaro <gu.tessaro@hotmail.com>
2026-07-13 04:32:27 -0300

    Arrumando exemplo em Cadastrar e Editar e criando a tabela no banco

--- EscolaDeCursosWebApp/Migrations/20260713072856_AddTurma.Designer.cs
+++ EscolaDeCursosWebApp/Migrations/20260713072856_AddTurma.Designer.cs
@@ -4,6 +4,7 @@ using EscolaDeCursosWebApp.Compartilhado.Infra.Orm;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EscolaDeCursosWebApp.Migrations
 {
     [DbContext(typeof(EscolaDeCursosDbContext))]
-    partial class EscolaDeCursosDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260713072856_AddTurma")]
+    partial class AddTurma
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EscolaDeCursosWebApp/Migrations/20260713072856_AddTurma.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EscolaDeCursosWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTurma : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBTurma",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    dataInicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    dataFim = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    vagasMaximas = table.Column<int>(type: "int", nullable: false),
+                    HorarioTurno = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false),
+                    cursoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    instrutorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBTurma", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TBTurma_Nome",
+                table: "TBTurma",
+                column: "nome");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBTurma");
+        }
+    }
+}
