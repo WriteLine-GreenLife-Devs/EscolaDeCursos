0338d9cbf4ebf268eb4ace16e1cf788350d70c87
Gustavo Tessaro <gu.tessaro@hotmail.com>
2026-07-13 03:21:02 -0300

    teste

--- EscolaDeCursosWebApp/Modulos/ModuloCurso/Dominio/StatusCurso.cs
+++ EscolaDeCursosWebApp/Modulos/ModuloCurso/Dominio/StatusCurso.cs
@@ -1,7 +1,7 @@
-namespace EscolaDeCursosWebApp.Modulos.ModuloCurso.Dominio; 
- 
-public enum StatusCurso 
-{ 
-    Ativo, 
-    Inativo 
+namespace EscolaDeCursosWebApp.Modulos.ModuloCurso.Dominio;
+
+public enum StatusCurso
+{
+    Ativo,
+    Inativo
 }
